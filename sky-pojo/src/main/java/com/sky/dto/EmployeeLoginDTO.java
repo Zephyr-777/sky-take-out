ab03dcc465c0f0796211662e60f421358e9306b6
Zephyr-777 <MengZH666@outlook.com>
2025-05-19 15:08:33 +0800

    项目框架搭建，swagger注解使用，接口测试

--- sky-pojo/src/main/java/com/sky/dto/EmployeeLoginDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/EmployeeLoginDTO.java
@@ -2,12 +2,18 @@ package com.sky.dto;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
-@ApiModel(description = "员工登录时传递的数据模型")
+@NoArgsConstructor
+@Builder //构造器 链式构造
+@AllArgsConstructor
+@ApiModel(description = "员工登录时传递的数据模型") //
 public class EmployeeLoginDTO implements Serializable {
 
     @ApiModelProperty("用户名")
@@ -15,5 +21,11 @@ public class EmployeeLoginDTO implements Serializable {
 
     @ApiModelProperty("密码")
     private String password;
+
+    @ApiModelProperty("姓名")
+    private String name;
+
+    @ApiModelProperty("jwt令牌")
+    private String token;
 
 }
