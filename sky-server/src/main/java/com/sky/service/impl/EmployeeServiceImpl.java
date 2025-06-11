1d7c65182353a3e0d1a8cd35d3e634e220893fb6
Zephyr-777 <MengZH666@outlook.com>
2025-06-11 14:39:50 +0800

    根据id查找员工，编辑员工信息

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -133,4 +133,26 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         employeeMapper.update(employee);
     }
+
+    /**
+     * 根据id查询员工信息
+     * @param id
+     * @return
+     */
+    @Override
+    public Employee getById(long id) {
+       Employee employee =  employeeMapper.getById(id);
+       employee.setPassword("****");//为了安全起见，返回的密码不显示
+       return employee;
+    }
+
+    public void update(EmployeeDTO employeeDTO) {
+        Employee employee = new Employee();
+        BeanUtils.copyProperties(employeeDTO, employee);
+
+        employee.setUpdateTime(LocalDateTime.now());
+        employee.setUpdateUser(BaseContext.getCurrentId());
+        
+        employeeMapper.update(employee); //对象属性转换
+    }
 }
