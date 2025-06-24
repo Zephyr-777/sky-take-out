8d6b52715f87e63a8f5c910681303a192383b12a
Zephyr-777 <MengZH666@outlook.com>
2025-06-24 15:33:16 +0800

    菜品分页查询

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,8 +1,11 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
 import com.sky.annotation.AutoFill;
+import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
 import com.sky.enumeration.OperationType;
+import com.sky.vo.DishVO;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -24,6 +27,15 @@ public interface DishMapper {
      * 插入菜品数据
      * @param dish 菜品实体
      */
-    @AutoFill(value = OperationType.INSERT)  // 自动填充
+    @AutoFill(value = OperationType.INSERT)  //  自动填充注解，指定操作类型为插入
     void insert(Dish dish);
+
+    /**
+     * 菜品分页查询
+     *
+     * @param dishPageQueryDTO
+     * @return
+     */
+    Page<DishVO> pageQuery(DishPageQueryDTO dishPageQueryDTO);
+
 }
