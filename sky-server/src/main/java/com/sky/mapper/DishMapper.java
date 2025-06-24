0de97ee55956f3a8bd9d3e779be6c86f1358e57e
Zephyr-777 <MengZH666@outlook.com>
2025-06-24 13:48:47 +0800

    新增菜品、文件上传功能

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,7 +1,12 @@
 package com.sky.mapper;
 
+import com.sky.annotation.AutoFill;
+import com.sky.entity.Dish;
+import com.sky.enumeration.OperationType;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Mapper
 public interface DishMapper {
@@ -14,4 +19,11 @@ public interface DishMapper {
     @Select("select count(id) from dish where category_id = #{categoryId}")
     Integer countByCategoryId(Long categoryId);
 
+
+    /**
+     * 插入菜品数据
+     * @param dish 菜品实体
+     */
+    @AutoFill(value = OperationType.INSERT)  // 自动填充
+    void insert(Dish dish);
 }
---
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -0,0 +1,53 @@
+package com.sky.service.impl;
+
+import com.sky.dto.DishDTO;
+import com.sky.entity.Dish;
+import com.sky.entity.DishFlavor;
+import com.sky.mapper.DishFlavorMapper;
+import com.sky.mapper.DishMapper;
+import com.sky.service.DishService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+
+@Slf4j
+@Service
+public class DishServiceImpl implements DishService {
+
+    @Autowired
+    private DishMapper dishMapper;
+    @Autowired
+    private DishFlavorMapper dIshFlavorMapper;
+    /**
+     * 新增菜品和口味数据
+     * @param dishDTO 菜品数据传输对象
+     */
+    @Transactional
+    @Override
+    public void saveWithFlavor(DishDTO dishDTO) {
+
+        Dish dish = new Dish();
+        BeanUtils.copyProperties(dishDTO, dish);
+
+        //向菜品表插入1条菜品数据
+        dishMapper.insert(dish);
+
+        //获取insert语句生成的主键值
+        long dishId = dish.getId();
+
+        //向菜品口味表插入多条数据(批量插入)
+        List<DishFlavor> flavors =  dishDTO.getFlavors();
+        if(flavors != null && !flavors.isEmpty()){
+            flavors.forEach(dishFlavor -> {
+                //设置口味对应的菜品id
+                dishFlavor.setDishId(dishId);
+            });
+            dIshFlavorMapper.insertBatch(flavors);
+        }
+    }
+}
